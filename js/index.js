05e5492363f27ec644684195a5ae39687c3067d1
Amy Chou <achou@DN0a1fe314.SUNet>
2018-02-17 15:43:18 -0800

    Fixed title

--- js/index.js
+++ js/index.js
@@ -2,35 +2,13 @@ function type() {
     $('#title').typeIt({
             speed: 100,
             autoStart: true
-        }).tiType('Aneesh Ashutosh<span class="emphasis">.</span>')
-        .tiPause(1000)
+        }).tiType('Wine Up<span class="emphasis">.</span>')
+        /*
+        .tiPause(200)
         .tiSettings({ speed: 200 })
         .tiDelete()
         .tiPause(800)
-        .tiSettings({ speed: 100 })
-
-        .tiType('Designer<span class="emphasis">.</span>')
-        .tiPause(1000)
-        .tiSettings({ speed: 200 })
-        .tiDelete()
-        .tiPause(800)
-        .tiSettings({ speed: 100 })
-
-        .tiType('Developer<span class="emphasis">.</span>')
-        .tiPause(1000)
-        .tiSettings({ speed: 200 })
-        .tiDelete()
-        .tiPause(800)
-        .tiSettings({ speed: 100 })
-
-        .tiType('Entrepreneur<span class="emphasis">.</span>')
-        .tiPause(1000)
-        .tiSettings({ speed: 200 })
-        .tiDelete()
-        .tiPause(800)
-        .tiSettings({ speed: 100 })
-
-        .tiType('Aneesh Ashutosh<span class="emphasis">.</span>');
+        .tiSettings({ speed: 100 })*/
 }
 
 function wine_quality() {
