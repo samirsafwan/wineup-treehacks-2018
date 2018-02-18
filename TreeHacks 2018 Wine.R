bdba004350120aa1eb8442deddd51aacd27d164f
Samir Safwan <samirsafwan@DN51ro4f.SUNet>
2018-02-17 19:27:26 -0800

    ass

--- TreeHacks 2018 Wine.R
+++ TreeHacks 2018 Wine.R
@@ -34,6 +34,8 @@ mean(pred != validation$quality)
 pred =predict(svm.fit, test)
 mean(pred != test$quality)
 
+save(svm.fit, file = "wine_support_vector_machine.rba")
+
 # Misclassification error is around 28%
 
 
@@ -44,6 +46,7 @@ vec = t(vec)
 as.numeric(as.matrix(predict(svm.fit, vec))[1])-3
 
 do.call(rep, input) %>% 
+  load("wine_support_vector_machine.rba")
   vec = data.frame(input)
   rownames(vec) = colnames(data)
   vec = t(vec)
